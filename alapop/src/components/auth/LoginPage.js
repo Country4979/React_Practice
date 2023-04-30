966dd0bfc200abe25c9d5f470af278d3215a409a
Javier Girón <115418134+Country4979@users.noreply.github.com>
2023-04-30 20:25:54 +0200

    Errors LoginPage Ok

--- alapop/src/components/auth/LoginPage.js
+++ alapop/src/components/auth/LoginPage.js
@@ -7,26 +7,52 @@ import CheckBox from './Checbox';
 import { useChecked } from './useChecked';
 import './LoginPage.css';
 import '../shared/Buttons.css';
+import { UseModal } from '../modals/UseModal';
+import Modal from '../modals/Modal';
 
 const LoginPage = ({ isLogged, onLogin, onLogout }) => {
     const [credentials, setCredentials] = useState({
         email: '',
         password: '',
     });
+
     const navigate = useNavigate();
+    const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState(null);
+    const [errorMs, setErrorMs] = useState('An error occurred while logging in')
+    const resetError = () => {
+        setError(null);
+    };
+    const [isOpenModalError, openModalError, closeModalError] = UseModal(false);
     const location = useLocation();
 
+    
     const handleSubmit = async (event) => {
-        
-           await login(credentials, checked);
-    
-           onLogin();
-    
-           const to = location.state?.from?.pathname || '/';
-           navigate(to);
-        
-
+        event.preventDefault();
+        try {
+            resetError();
+            setIsLoading(true);
+            await login(credentials, checked);
+            //Logged in:
+            onLogin();
+            // Redirect to pathname
+            const to = location.state?.from?.pathname || '/';
+            navigate(to);
+        } catch (error) {
+            console.log(error.response.status)
+            setError(error);
+            if(error.response.status === 401) {
+                console.log('error 401')
+                setErrorMs('Incorrect username or password')   
+                openModalError()
+            }
+            openModalError();
+            
+        }finally {
+            setIsLoading(false);
+        }
     };
+    console.log(errorMs)
 
     const handleChange = (event) => {
         setCredentials({
@@ -48,6 +74,23 @@ const LoginPage = ({ isLogged, onLogin, onLogout }) => {
 
     return (
         <>
+            <Modal
+                name='error'
+                isOpen={isOpenModalError}
+                closeModal={closeModalError}
+                
+            >
+                <h3 className='modalErrorH3'>
+                    {errorMs}.
+                </h3>
+                <Button
+                    className='noDeleteButton'
+                    variant='primary'
+                    onClick={closeModalError}
+                >
+                    Please try again...
+                </Button>
+            </Modal>
             <div className='infoContainer'>
                 <div className='leftSide' id='leftSide'>
                     <h1 id='textLogin'>Already Logged?</h1>
